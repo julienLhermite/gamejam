c9b8300023d479c5bd7f1f9804f17e6093939211
Julien Lhermite <julien.lhermite@gmail.com>
2017-04-08 13:37:04 +0200

    yolo

--- pygame_sandbox.py
+++ pygame_sandbox.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.locals import *
 
-# elmt de la bibliothèque
+# elmt de la bibliotheque
 # -display
 # -mixer
 # -draw
@@ -14,14 +14,16 @@ from pygame.locals import *
 # initialise les modules pygame
 pygame.init()
 
+print(pygame.image.get_extended())
+
 # renvoie un objet de classe surface, pour l'instant surface vierge donc noir
 # fenetre = pygame.display.set_mode((640, 480))
-fenetre = pygame.display.set_mode((640, 480), FULLSCREEN)
+fenetre = pygame.display.set_mode((640, 480), RESIZABLE)
 
-fond = pygame.image.load("background.bmp")
-# fond = pygame.image.load("background.bmp").convert()
+fond = pygame.image.load("background.jpg")
+# fond = pygame.image.load("background.jpg").convert()
 
-fenetre.blit(fond, (0,0))
+fenetre.blit(fond, (0, 0))
 # Variable qui continue la boucle si = 1, stoppe si = 0
 continuer = 1
 
